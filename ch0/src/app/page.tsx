3ac379686dccd6d20250849bda656256b8b44a7f
김신영 <ksyq12@gmail.com>
2023-12-09 19:20:27 +0900

    css module

--- ch0/src/app/page.tsx
+++ ch0/src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import zLogo from '../../public/zlogo.png'
+import zLogo from '../../public/zlogo.png';
+
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <div className={styles.left}>
         <Image src={zLogo} alt="logo" />
       </div>
-      <div className={StyleSheet.right}>
+      <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
         <Link href="/i/flow/signup" className={styles.signup}>
@@ -19,6 +21,6 @@ export default function Home() {
           로그인
         </Link>
       </div>
-    </div>
+    </>
   );
 }
