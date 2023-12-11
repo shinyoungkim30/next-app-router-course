7dfc729682a31d0c01b2d73f126303ac3e677b9b
김신영 <ksyq12@gmail.com>
2023-12-11 19:18:38 +0900

    nav menu

---
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import styles from './LogoutButton.module.css';
+
+export default function LogoutButton() {
+  const me = {
+    // 임시로 내 정보 있는것처럼
+    id: 'zerohch0',
+    nickname: '제로초',
+    image: '/5Udwvqim.jpg',
+  };
+
+  const onLogout = () => {};
+
+  return (
+    <button className={styles.logOutButton} onClick={onLogout}>
+      <div className={styles.logOutUserImage}>
+        <img src={me.image} alt={me.id} />
+      </div>
+      <div className={styles.logOutUserName}>
+        <div>{me.nickname}</div>
+        <div>@{me.id}</div>
+      </div>
+    </button>
+  );
+}
