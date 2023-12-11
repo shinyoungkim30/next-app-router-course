9789b26aa6b3c284209707ae7e3c5de3bd1d90e4
김신영 <ksyq12@gmail.com>
2023-12-11 16:25:42 +0900

    commit

--- ch0/src/app/(beforeLogin)/login/page.tsx
+++ ch0/src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,18 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Main from '../_component/Main';
 
 export default function Login() {
-  return redirect('/i/flow/login');
+  const router = useRouter();
+
+  router.replace('/i/flow/login');
+
+  return <Main />;
 }
+
+// 1) router.push (뒤로가기 클릭시 login -> i/flow/login 무한 반복)
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+
+// 2) router.replace (이동 후 이전 경로를 삭제, 뒤로가기 클릭시 localhost:3000으로)
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
