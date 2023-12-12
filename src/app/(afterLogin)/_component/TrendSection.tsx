168d6e2996e61fa79ed4e633df68d91e7f1698b3
김신영 <ksyq12@gmail.com>
2023-12-12 15:27:52 +0900

    explore page

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,14 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Trend from './Trend';
 import styles from './TrendSection.module.css';
 
 export default function TrendSection() {
+  const pathname = usePathname();
+
+  if (pathname === '/explore') return null;
+
   return (
     <div className={styles.trendBg}>
       <div className={styles.trend}>
